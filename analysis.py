028e0d083cfebc6dbc387d8866c0abf28e82d007
Anabele-Linda Pardi <anabelepardi@gmail.com>
2023-02-28 09:39:29 +0100

    Write out json file with the dictionary
    Additionally I renamed the dictionary fields as well.

--- analysis.py
+++ analysis.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 
 data = {}
@@ -8,8 +9,9 @@ for index, row in df.iterrows():
     if pd.notna(row[5]) and pd.notna(row[6]):  # check if both columns have values
         # add the row to the dictionary
         key = row[2]  # use the third column as the key
-        data[key] = {'column_4': row[3], 'column_6': row[5], 'column_7': row[6]}
+        data[key] = {'loc': row[3], 'NO2': row[5], 'O3': row[6]}
 
-# print the dictionary
-print(df.size)
+# write the dictionary to a JSON file
+with open('NO2-O3.json', 'w') as outfile:
+    json.dump(data, outfile)
 
